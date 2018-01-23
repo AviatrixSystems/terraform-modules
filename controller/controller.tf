f1872ea0d18a055bffd5dbc216e0604f0ee5c6f9
Nate Williams <nate@yogotech.com>
2018-01-22 19:47:52 -0700

    Breakup the functionality into multiple files

--- controller/controller.tf
+++ controller/controller.tf
@@ -1,64 +1,3 @@
-variable "region" {}
-variable "vpc" {}
-variable "subnet" {}
-variable "keypair" {}
-variable "ec2role" {}
-
-variable "images" {
-  type = "map"
-
-  default = {
-    us-east-1      = "ami-690c467e"
-    us-east-2      = "ami-811248e4"
-    us-west-1      = "ami-43206823"
-    us-west-2      = "ami-03558e63"
-    ca-central-1   = "ami-2dc57749"
-    eu-central-1   = "ami-70c43a1f"
-    eu-west-1      = "ami-33d69440"
-    eu-west-2      = "ami-a1272dc5"
-    ap-south-1     = "ami-970672f8"
-    ap-northeast-1 = "ami-7308d212"
-    ap-northeast-2 = "ami-93d400fd"
-    ap-southeast-1 = "ami-f4258297"
-    ap-southeast-2 = "ami-921624f1"
-    sa-east-1      = "ami-bcf66bd0"
-  }
-}
-
-#Define the region
-provider "aws" {
-  region = "${var.region}"
-}
-
-resource "aws_security_group" "AviatrixSecurityGroup" {
-  name        = "AviatrixSecurityGroup"
-  description = "Aviatrix - Controller Security Group"
-  vpc_id      = "${var.vpc}"
-
-  tags {
-    Name      = "AviatrixSecurityGroup"
-    Createdby = "Terraform+Aviatrix"
-  }
-}
-
-resource "aws_security_group_rule" "ingress_rule" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.AviatrixSecurityGroup.id}"
-}
-
-resource "aws_security_group_rule" "egress_rule" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.AviatrixSecurityGroup.id}"
-}
-
 resource "aws_eip" "controller_eip" {
   vpc = true
 }
@@ -105,12 +44,4 @@ resource "aws_instance" "aviatrixcontroller" {
     Name      = "AviatrixController"
     Createdby = "Terraform+Aviatrix"
   }
-}
-
-output "private-ip" {
-  value = "${aws_instance.aviatrixcontroller.private_ip}"
-}
-
-output "public-ip" {
-  value = "${aws_instance.aviatrixcontroller.public_ip}"
 }
---
+++ controller/vars.tf
@@ -0,0 +1,32 @@
+variable "region" {}
+
+variable "vpc" {}
+
+variable "subnet" {}
+
+variable "keypair" {}
+
+variable "ec2role" {}
+
+#
+# Defaults
+#
+
+variable "images" {
+  default = {
+    us-east-1      = "ami-690c467e"
+    us-east-2      = "ami-811248e4"
+    us-west-1      = "ami-43206823"
+    us-west-2      = "ami-03558e63"
+    ca-central-1   = "ami-2dc57749"
+    eu-central-1   = "ami-70c43a1f"
+    eu-west-1      = "ami-33d69440"
+    eu-west-2      = "ami-a1272dc5"
+    ap-south-1     = "ami-970672f8"
+    ap-northeast-1 = "ami-7308d212"
+    ap-northeast-2 = "ami-93d400fd"
+    ap-southeast-1 = "ami-f4258297"
+    ap-southeast-2 = "ami-921624f1"
+    sa-east-1      = "ami-bcf66bd0"
+  }
+}
