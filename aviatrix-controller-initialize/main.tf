d095fcfad35dab91ee61b973eed309a7099068c0
Zhiqiang Jin <zjin82@gmail.com>
2019-01-15 11:22:12 -0800

    fix a typo

--- aviatrix-controller-initialize/main.tf
+++ aviatrix-controller-initialize/main.tf
@@ -23,15 +23,17 @@ resource "aws_iam_role_policy_attachment" "attach-policy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+data "aws_region" "current" {}
+
 resource "aws_lambda_function" "lambda" {
-  s3_bucket        = "aviatrix-lamba-us-west-1"
-  s3_key           = "https://s3-ap-northeast-1.amazonaws.com/aviatrix-lambda-ap-northeast-1/run_controller_init_setup.zip"
-  function_name    = "AvxLambda"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "run_controller_init_setup.lambda_handler"
-  runtime          = "python3.7"
-  description      = "MANAGED BY TERRAFORM"
-  timeout          = 300
+  s3_bucket     = "aviatrix-lambda-${data.aws_region.current.name}"
+  s3_key        = "run_controller_init_setup.zip"
+  function_name = "AvxLambda"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "run_controller_init_setup.lambda_handler"
+  runtime       = "python3.7"
+  description   = "MANAGED BY TERRAFORM"
+  timeout       = 300
 }
 
 data "aws_lambda_invocation" "example" {
@@ -40,19 +42,19 @@ data "aws_lambda_invocation" "example" {
   input = <<JSON
 { "ResourceProperties":
 {
-  "PrefixStringParam"               : "avx",
-  "AWS_Account_ID"                  : "${var.aws_account_id}",
-  "KeywordForCloudWatchLogParam"    : "avx-log",
-  "DelimiterForCloudWatchLogParam"  : "---",
-  "ControllerPublicIpParam"         : "${var.public_ip}",
-  "AviatrixApiVersionParam"         : "v1",
-  "AviatrixApiRouteParam"           : "api/",
-  "ControllerPrivateIpParam"        : "${var.private_ip}",
-  "ControllerAdminPasswordParam"    : "${var.admin_password}",
-  "ControllerAdminEmailParam"       : "${var.admin_email}",
-  "ControllerVersionParam"          : "latest",
-  "ControllerAccessAccountNameParam": "${var.access_account_name}",
-  "AviatrixCustomerLicenseIdParam"  : "customer_license_id"
+  "PrefixStringParam"                : "avx",
+  "AWS_Account_ID"                   : "${var.aws_account_id}",
+  "KeywordForCloudWatchLogParam"     : "avx-log",
+  "DelimiterForCloudWatchLogParam"   : "---",
+  "ControllerPublicIpParam"          : "${var.public_ip}",
+  "AviatrixApiVersionParam"          : "v1",
+  "AviatrixApiRouteParam"            : "api/",
+  "ControllerPrivateIpParam"         : "${var.private_ip}",
+  "ControllerAdminPasswordParam"     : "${var.admin_password}",
+  "ControllerAdminEmailParam"        : "${var.admin_email}",
+  "ControllerVersionParam"           : "latest",
+  "ControllerAccessAccountNameParam" : "${var.access_account_name}",
+  "AviatrixCustomerLicenseIdParam"   : "${var.customer_license_id}"
 }
 }
 JSON
