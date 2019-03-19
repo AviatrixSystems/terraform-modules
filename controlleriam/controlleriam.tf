e92f40d02f1b5f332e640a422becba36bb48151e
Zhiqiang Jin <zjin82@gmail.com>
2019-03-19 10:28:01 -0700

    Updated READ.md for controller build to add description for some inputs

--- controlleriam/controlleriam.tf
+++
@@ -1,6 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-module "iam_roles" {
-  source            = "github.com/AviatrixSystems/terraform-modules.git/aviatrix-controller-iam-roles"
-  master-account-id = "${data.aws_caller_identity.current.account_id}"
-}
--- controllerinit/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  access_key = "AKIAJWRLT5ASX7EILWEA"
-  secret_key = "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"
-  region     = "us-west-1"
-}
-
