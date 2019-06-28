6a5269c7725467304b6ca23e5f28ae68691ec029
ZHIQIANG JIN <44539929+zhiqiangjin@users.noreply.github.com>
2019-06-28 15:16:07 -0700

    Make termination protection be a variable to add options (#31)

--- aviatrix-controller-build/main.tf
+++ aviatrix-controller-build/main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "aviatrixcontroller" {
   instance_type           = "${var.instance_type}"
   key_name                = "${var.keypair}"
   iam_instance_profile    = "${var.ec2role}"
-  disable_api_termination = true
+  disable_api_termination = "${var.termination_protection}"
 
   network_interface {
     network_interface_id = "${element(aws_network_interface.eni-controller.*.id, count.index)}"
