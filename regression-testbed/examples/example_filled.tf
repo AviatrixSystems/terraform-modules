1349f4b6f5ed0bf96548872a6fb36f8ab21baf53
unknown <matthew.dumanig@gmail.com>
2019-10-15 16:19:43 -0700

    update example files

--- regression-testbed/examples/example_filled.tf
+++ regression-testbed/examples/example_filled.tf
@@ -1,28 +1,24 @@
-module "regression-testbed" {
-  source = "./regression-testbed"
-	termination_protection			= false
-  resource_name_label         = "regression"
 
-  # Provider
-  # AWS Primary
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# AWS Basic Creation module
+# - VPCS in each region with Ubuntu instances
+# - Aviatrix Controller (optional)
+# - Windows instance for RDP (optional)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+module "testbed-basic" {
+  source = "./regression-testbed/modules/testbed-basic"
+	termination_protection			= local.termination_protection
+  resource_name_label         = local.resource_name_label
+
+  # AWS Primary Account
   aws_primary_acct_access_key = ""
   aws_primary_acct_secret_key = ""
 
-  # AWS Cross
-  cross_aws_acct_access_key   = ""
-  cross_aws_acct_secret_key   = ""
-
-  # ARM
-  arm_subscription_id         = ""
-  arm_tenant_id               = ""
-  arm_client_id               = ""
-  arm_client_secret           = ""
-
   # AWS VPC setup
 	# example for 2 vpcs
-  vpc_public_key              = ""
-  pub_hostnum                 = 20
-  pri_hostnum                 = 30
+  vpc_public_key              = local.public_key
+  pub_hostnum                 = local.pub_hostnum
+  pri_hostnum                 = local.pri_hostnum
 
   # US West 1
 	# each variable for the cidr are lists
@@ -71,31 +67,12 @@ module "regression-testbed" {
   pri_subnet_az_east2         = ["us-east-2a", "us-east-2b"]
   ubuntu_ami_east2            = "ami-09f0299359c12ab7c"
 
-  # AWS Cross Account
-  cross_aws_region            = "us-east-2"
-  vpc_count_cross_aws         = 2
-  vpc_cidr_cross_aws          = ["10.9.0.0/16", "10.10.0.0/16"]
-  pub_subnet1_cidr_cross_aws  = ["10.9.0.0/24", "10.10.3.0/24"]
-  pub_subnet2_cidr_cross_aws  = ["10.9.1.0/24", "10.10.4.0/24"]
-  pri_subnet_cidr_cross_aws   = ["10.9.2.0/24", "10.10.5.0/24"]
-  pub_subnet1_az_cross_aws    = ["us-east-2a", "us-east-2b"]
-  pub_subnet2_az_cross_aws    = ["us-east-2a", "us-east-2b"]
-  pri_subnet_az_cross_aws     = ["us-east-2a", "us-east-2b"]
-  ubuntu_ami_cross_aws        = "ami-09f0299359c12ab7c"
-
-  # ARM VNET
-  arm_region                  = "East US"
-  vnet_count_arm              = 2
-  vnet_cidr_arm               = ["10.20.0.0/16", "10.30.0.0/16"]
-  pub_subnet_cidr_arm         = ["10.20.1.0/24", "10.30.3.0/24"]
-  pri_subnet_cidr_arm         = ["10.20.2.0/24", "10.30.4.0/24"]
-
 	# AWS VPC for controller
-  deploy_controller           = true/false
-	controller_region           = "us-west-1"
+  deploy_controller           = true
+  controller_region           = "us-west-1"
 	controller_vpc_cidr         = "10.22.0.0/16"
 	controller_subnet_cidr			= "10.22.23.0/24"
-	controller_public_key       = ""
+	controller_public_key       = local.public_key
 	controller_sg_source_ip     = "0.0.0.0/0"
 
 	# Aviatrix controller
@@ -109,20 +86,83 @@ module "regression-testbed" {
   windows_region              = "us-east-1"
   windows_vpc_cidr            = "20.5.0.0/16"
   windows_subnet_cidr         = "20.5.1.0/24"
-  windows_public_key          = ""
+  windows_public_key          = local.public_key
   windows_ami                 = "ami-0069635df219ce9e5"
 	windows_sg_source_ip        = "0.0.0.0/0"
 }
 
-## Leave following provider/module commented before initial terraform apply
-## After successful terraform apply, uncomment:
-## provider "aviatrix"
-## module "aviatrix-access-accounts"
-#
+## AWS Cross account and Azure Vnet modules are optional.
+## If you want to include into testbed, uncomment the modules and providers.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# AWS Cross Account Module (Optional)
+# - VPC with Ubuntu instance in alternate AWS account
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#provider "aws" {
+#	alias									 = "cross_aws_acc"
+#  version       				 = "~> 2.7"
+#  region        				 = "us-east-2"
+#	access_key    				 = ""
+#	secret_key    				 = ""
+#}
+#module "aws-cross-acct" {
+#  source                 = "./regression-testbed/modules/testbed-vpcs"
+#  providers = {
+#    aws = aws.cross_aws_acc
+#  }
+#  vpc_count              = 1
+#  resource_name_label    = local.resource_name_label
+#	pub_hostnum						 = local.pub_hostnum
+#  pri_hostnum            = local.pri_hostnum
+#  vpc_cidr         			 = ["10.9.0.0/16", "10.10.0.0/16"]
+#  pub_subnet1_cidr       = ["10.9.0.0/24", "10.10.3.0/24"]
+#  pub_subnet2_cidr       = ["10.9.1.0/24", "10.10.4.0/24"]
+#  pri_subnet_cidr        = ["10.9.2.0/24", "10.10.5.0/24"]
+#  pub_subnet1_az      	 = ["us-east-2a", "us-east-2b"]
+#  pub_subnet2_az      	 = ["us-east-2a", "us-east-2b"]
+#  pri_subnet_az       	 = ["us-east-2a", "us-east-2b"]
+#  ubuntu_ami             = "ami-09f0299359c12ab7c"
+#  public_key             = local.public_key
+#	termination_protection = local.termination_protection
+#}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Azure Vnet Module (Optional)
+# - VNETs with Ubuntu instances in specified region
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#provider "azurerm" {
+#	version = "1.34"
+#	subscription_id 			= ""
+#	tenant_id 						= ""
+#	client_id 						= ""
+#	client_secret   			= ""
+#}
+#module "arm-vnet" {
+#	source 								= "./regression-testbed/modules/testbed-vnet-arm"
+#	region 			 					= "East US"
+#	vnet_count 						= 1
+#	resource_name_label 	= local.resource_name_label
+#  pub_hostnum						= local.pub_hostnum
+#  pri_hostnum           = local.pri_hostnum
+#  vnet_cidr             = ["10.20.0.0/16", "10.30.0.0/16"]
+#	pub_subnet_cidr       = ["10.20.1.0/24", "10.30.3.0/24"]
+#	pri_subnet_cidr       = ["10.20.2.0/24", "10.30.4.0/24"]
+#	public_key 						= local.public_key
+#}
+
+## Leave following provider/module commented before initial terraform apply.
+## After successful terraform apply, uncomment.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Aviatrix Modules
+# **Create these modules last!**
+# - Creating additional Aviatrix Access accounts
+# - Simulate Onprem connection with Aviatrix S2C
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #provider "aviatrix" {
-#	controller_ip	= module.regression-testbed.controller_public_ip
-#	username			= element(module.regression-testbed.controller_username_password, 0)
-#	password			= element(module.regression-testbed.controller_username_password, 1)
+#	controller_ip	= module.testbed-basic.controller_public_ip
+#	username			= element(module.testbed-basic.controller_username_password, 0)
+#	password			= element(module.testbed-basic.controller_username_password, 1)
 #}
 #module "aviatrix-access-accounts" {
 #  source                = "./regression-testbed/modules/testbed-aviatrix-accounts"
@@ -136,3 +176,40 @@ module "regression-testbed" {
 #  gcp_id                   =
 #  gcp_credentials_filepath = ""
 #}
+#provider "aws" {
+#	alias					= "onprem"
+#  version       = "~> 2.7"
+#  region        = "us-west-2"
+#  access_key 		= ""
+#  secret_key 		= ""
+#}
+#module "testbed-onprem" {
+#	source = "./regression-testbed/modules/testbed-onprem"
+#	providers = {
+#		aws = aws.onprem
+#	}
+#  account_name           = module.testbed-basic.primary_access_account
+#
+#  onprem_vpc_cidr        = "10.70.0.0/16"
+#  pub_subnet_cidr        = "10.70.5.0/24"
+#  pri_subnet_cidr        = "10.70.36.0/24"
+#  pub_subnet_az          = "us-west-2b"
+#  pri_subnet_az          = "us-west-2b"
+#  pub_hostnum            = local.pub_hostnum
+#  pri_hostnum            = local.pri_hostnum
+#
+#  termination_protection = local.termination_protection
+#  public_key             = local.public_key
+#  ubuntu_ami             = "ami-0eaedb5f9e4e556f4"
+#}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# LOCALS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+locals {
+	resource_name_label	 	 = "regression"
+	pub_hostnum						 = 20
+	pri_hostnum						 = 40
+	public_key 						 = file("~/.ssh/id_rsa.pub")
+	termination_protection = false
+}
--- regression-testbed/examples/testbed_outputs.tf
+++ regression-testbed/examples/testbed_outputs.tf
@@ -1,40 +1,136 @@
-# Outputs for TF Regression testbed
+## Outputs for TF Regression testbed
+## Uncomment the outputs for the modules you will be using.
+## Comment Aviatrix outputs after destroying the modules,
+## before destroying the rest of the resources.
 
-# AWS VPCs
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# AWS Basic Creation module
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# AWS Vpcs
 output "us-west-1" {
-  value = [module.regression-testbed.west1_vpc_info, module.regression-testbed.west1_subnet_info, module.regression-testbed.west1_ubuntu_info]
+  value = concat(
+    module.testbed-basic.west1_vpc_info,
+    module.testbed-basic.west1_subnet_info,
+    module.testbed-basic.west1_ubuntu_info
+  )
 }
 output "us-west-2" {
-  value = [module.regression-testbed.west2_vpc_info, module.regression-testbed.west2_subnet_info, module.regression-testbed.west2_ubuntu_info]
+  value = concat(
+    module.testbed-basic.west2_vpc_info,
+    module.testbed-basic.west2_subnet_info,
+    module.testbed-basic.west2_ubuntu_info
+  )
 }
 output "us-east-1" {
-  value = [module.regression-testbed.east1_vpc_info, module.regression-testbed.east1_subnet_info, module.regression-testbed.east1_ubuntu_info]
+  value = concat(
+    module.testbed-basic.east1_vpc_info,
+    module.testbed-basic.east1_subnet_info,
+    module.testbed-basic.east1_ubuntu_info
+  )
 }
 output "us-east-2" {
-  value = [module.regression-testbed.east2_vpc_info, module.regression-testbed.east2_subnet_info, module.regression-testbed.east2_ubuntu_info]
-}
-output "cross-aws-vpc" {
-  value = [module.regression-testbed.cross_aws_vpc_info, module.regression-testbed.cross_aws_subnet_info, module.regression-testbed.cross_aws_ubuntu_info]
-}
-output "arm-vnet" {
-  value = [module.regression-testbed.arm_vnet_info, module.regression-testbed.arm_subnet_info, module.regression-testbed.arm_ubuntu_info]
+  value = concat(
+    module.testbed-basic.east2_vpc_info,
+    module.testbed-basic.east2_subnet_info,
+    module.testbed-basic.east2_ubuntu_info
+  )
 }
 
 # Aviatrix Controller
 output "controller_public_ip" {
- value = module.regression-testbed.controller_public_ip
+ value = module.testbed-basic.controller_public_ip
 }
 output "controller_key" {
- value = module.regression-testbed.controller_key
+ value = module.testbed-basic.controller_key
 }
 output "controller_username_password" {
- value = module.regression-testbed.controller_username_password
+ value = module.testbed-basic.controller_username_password
 }
 
 # Windows instance
 output "windows_public_ip" {
- value = module.regression-testbed.windows_public_ip
+ value = module.testbed-basic.windows_public_ip
 }
 output "windows_key" {
- value = module.regression-testbed.windows_key
+ value = module.testbed-basic.windows_key
 }
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## AWS Cross Account Module (Optional)
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#output "cross_aws_vpc_info" {
+#	value = [
+#    module.aws-cross-acct.vpc_name,
+#    module.aws-cross-acct.vpc_id
+#  ]
+#}
+#output "cross_aws_subnet_info" {
+#	value = [
+#    module.aws-cross-acct.subnet_name,
+#    module.aws-cross-acct.subnet_cidr
+#  ]
+#}
+#output "cross_aws_ubuntu_info" {
+#	value = [
+#    module.aws-cross-acct.ubuntu_name,
+#    module.aws-cross-acct.ubuntu_id,
+#    module.aws-cross-acct.ubuntu_public_ip,
+#    module.aws-cross-acct.ubuntu_private_ip
+#  ]
+#}
+#
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Azure Vnet Module (Optional)
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#output "arm_vnet_info" {
+#	value = [
+#    module.arm-vnet.vnet_name,
+#    module.arm-vnet.vnet_id
+#  ]
+#}
+#output "arm_subnet_info" {
+#	value = [
+#    module.arm-vnet.subnet_name,
+#    module.arm-vnet.subnet_cidr
+#  ]
+#}
+#output "arm_ubuntu_info" {
+#	value = [
+#    module.arm-vnet.ubuntu_name,
+#    module.arm-vnet.ubuntu_id,
+#    module.arm-vnet.ubuntu_public_ip,
+#    module.arm-vnet.ubuntu_private_ip
+#  ]
+#}
+#
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Aviatrix Onprem Module
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#output "onprem_vpc_info" {
+#  value = module.testbed-onprem.onprem_vpc_info
+#}
+#output "onprem_subnet_info" {
+#  value = [
+#    module.testbed-onprem.onprem_public_subnet_info,
+#    module.testbed-onprem.onprem_private_subnet_info
+#  ]
+#}
+#output "onprem_ubuntu_info" {
+#  value = [
+#    module.testbed-onprem.onprem_public_ubuntu_info,
+#    module.testbed-onprem.onprem_private_ubuntu_info
+#  ]
+#}
+#output "onprem_aviatrix_gw_info" {
+#  value = module.testbed-onprem.onprem_aviatrix_gw_info
+#}
+#output "onprem_aws_resource_info" {
+#  value = [
+#    module.testbed-onprem.onprem_vgw_info,
+#    module.testbed-onprem.onprem_cgw_info,
+#    module.testbed-onprem.onprem_vpn_info
+#  ]
+#}
+#output "onprem_s2c_info" {
+#  value = module.testbed-onprem.onprem_s2c_info
+#}
